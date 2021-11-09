3d09bcab59f26dc5e76bf7c918c5c274a65bbc55
Ant Man <34754995+aaronamendez@users.noreply.github.com>
2021-11-09 17:15:19 -0500

    Finished Tasks 1-5, moving onto 6 now

--- index.js
+++ index.js
@@ -23,15 +23,20 @@ console.log(finals2014[0]["Win conditions"]);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use getFinals to do the following:
-1. Receive data as a parameter
+1. Receive data as a parameter (done)
 2. Return an array of objects with the data of the teams that made it to the final stage
 
 hint - you should be looking at the stage key inside of the objects
 */
 
-function getFinals(/* code here */) {
-	/* code here */
+function getFinals(data) {
+	const allFinals = data.filter(function (item) {
+		return item.Stage === "Final";
+	});
+	return allFinals;
 }
+
+// console.log(getFinals(fifaData));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Use the higher-order function called getYears to do the following: 
@@ -39,20 +44,27 @@ Use the higher-order function called getYears to do the following:
 2. Receive a callback function getFinals from task 2 
 3. Return an array called years containing all of the years in the getFinals data set*/
 
-function getYears(/* code here */) {
+function getYears(array, callback) {
 	/* code here */
+	return callback(array).map((item) => item.Year);
 }
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Use the higher-order function getWinners to do the following:  
-1. Receives an array
-2. Receives the callback function getFinals from task 2 
+1. Receives an array //
+2. Receives the callback function getFinals from task 2 //
 3. Determines the winner (home or away) of each `finals` game. 
 4. Returns the names of all winning countries in an array called `winners` */
 
-function getWinners(/* code here */) {
-	/* code here */
+function getWinners(array, callback) {
+	return callback(array).map((item) =>
+		item["Home Team Goals"] > item["Away Team Goals"]
+			? item["Home Team Name"]
+			: item["Away Team Name"]
+	);
 }
+
+console.log(getWinners(fifaData, getFinals));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use the higher-order function getWinnersByYear to do the following:
